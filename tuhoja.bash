0d6945eb94b3f393ad2f46972d35521443301d9a
Hermanni Pinomäki <63274642+Poliisi5@users.noreply.github.com>
2024-09-18 10:43:57 +0300

    Create tuhoja.bash

---
+++ tuhoja.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+TEMPFILE=/tmp/counter.tmp
+echo 0 > $TEMPFILE
+for (( ; ; ))
+do
+  COUNTER=$[$(cat $TEMPFILE) + 1]
+  echo $COUNTER > $TEMPFILE
+  touch $COUNTER
+  echo $COUNTER
+done
