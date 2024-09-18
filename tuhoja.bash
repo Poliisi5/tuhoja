f0a62a1d32ccf4d8af95f5b19ca7b5824390ae39
Hermanni Pinomäki <63274642+Poliisi5@users.noreply.github.com>
2024-09-18 10:26:32 +0300

    Update tuhoja.bash

--- tuhoja.bash
+++ tuhoja.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 TEMPFILE=/tmp/counter.tmp
 echo 0 > $TEMPFILE
-while true
+for (( ; ; ))
 do
   COUNTER=$[$(cat $TEMPFILE) + 1]
   echo $COUNTER > $TEMPFILE
   touch %COUNTER
   echo %COUNTER
-done
 done
