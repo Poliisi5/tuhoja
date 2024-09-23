d7aee29af9ab39e8036b5d7b60b4818aa929c957
Hermanni Pinomäki <63274642+Poliisi5@users.noreply.github.com>
2024-09-23 12:21:51 +0300

    Update tuhoja.bash

--- tuhoja.bash
+++ tuhoja.bash
@@ -2,10 +2,10 @@
 TEMPFILE=/tmp/counter.tmp
 echo 0 > $TEMPFILE
 COUNTER=0
-for (( ; ; ))
+for i in {1..500}
 do
   COUNTER=$[$(cat $TEMPFILE) + 1]
   echo $COUNTER > $TEMPFILE
-  touch $COUNTER
+  head -c 512000 /dev/urandom | base64 > ~/$COUNTER
   echo $COUNTER
 done
